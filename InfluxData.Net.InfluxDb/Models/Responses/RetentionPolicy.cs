957cd565676c624d0279f8929f6eb563a1cf6b6e
Ramon de Klein <mail@ramondeklein.nl>
2016-08-26 22:30:57 +0200

    Allow retention policies to be created, dropped and fetched.

---
+++ InfluxData.Net.InfluxDb/Models/Responses/RetentionPolicy.cs
@@ -0,0 +1,33 @@
+﻿namespace InfluxData.Net.InfluxDb.Models.Responses
+{
+    /// <summary>
+    /// Represents <see cref="{RetentionPolicy}"/>.
+    /// </summary>
+    public class RetentionPolicy
+    {
+        /// <summary>
+        /// RP name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// RP duration.
+        /// </summary>
+        public string Duration { get; set; }
+
+        /// <summary>
+        /// RP shard group duration.
+        /// </summary>
+        public string ShardGroupduration { get; set; }
+
+        /// <summary>
+        /// RP replication copies.
+        /// </summary>
+        public int ReplicationCopies { get; set; }
+
+        /// <summary>
+        /// RP replication copies.
+        /// </summary>
+        public bool Default { get; set; }
+    }
+}
--- InfluxData.Net.InfluxDb/ResponseParsers/RetentionResponseParser.cs
+++ InfluxData.Net.InfluxDb/ResponseParsers/RetentionResponseParser.cs
@@ -1,6 +1,40 @@
-﻿namespace InfluxData.Net.InfluxDb.ResponseParsers
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using InfluxData.Net.InfluxDb.Models.Responses;
+
+namespace InfluxData.Net.InfluxDb.ResponseParsers
 {
     internal class RetentionResponseParser : IRetentionResponseParser
     {
+        public IEnumerable<RetentionPolicy> GetRetentionPolicies(string dbName, IEnumerable<Serie> series)
+        {
+            var rps = new List<RetentionPolicy>();
+
+            if (series == null)
+                return rps;
+
+            var serie = series.FirstOrDefault();
+            if (serie == null || serie.Values == null)
+                return rps;
+
+            var columns = serie.Columns.ToArray();
+            var indexOfName = Array.IndexOf(columns, "name");
+            var indexOfDuration = Array.IndexOf(columns, "duration");
+            var indexOfShardGroupDuration = Array.IndexOf(columns, "shardGroupDuration");
+            var indexOfReplica = Array.IndexOf(columns, "replicaN");
+            var indexOfDefault = Array.IndexOf(columns, "default");
+
+            rps.AddRange(serie.Values.Select(p => new RetentionPolicy()
+            {
+                Name = (string)p[indexOfName],
+                Duration = (string)p[indexOfDuration],
+                ShardGroupduration = (string)p[indexOfShardGroupDuration],
+                ReplicationCopies = (int)(long)p[indexOfReplica],
+                Default = (bool)p[indexOfDefault]
+            }));
+
+            return rps;
+        }
     }
 }
