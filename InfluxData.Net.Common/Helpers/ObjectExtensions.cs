3f3ed9fa594f94a4f48098e3fe51fe2a48557cf7
pootzko <pootzko@gmail.com>
2016-11-29 23:40:52 +0000

    Dropped support for win phone.

--- InfluxData.Net.Common/Helpers/ObjectExtensions.cs
+++ InfluxData.Net.Common/Helpers/ObjectExtensions.cs
@@ -26,20 +26,6 @@ namespace InfluxData.Net.Common.Helpers
         }
 
         /// <summary>
-        /// Converts <see cref="{TimeUnit}"/> enum to param InfluxDb-ready string value.
-        /// </summary>
-        /// <param name="value"><see cref="{TimeUnit}"/> enum.</param>
-        /// <returns>InfluxDb-ready time unit string value.</returns>
-        public static string GetParamValue(this Enum value)
-        {
-            var valueFields = value.GetType().GetField(value.ToString());
-            var paramValue = (ParamValueAttribute[])(valueFields)
-                        .GetCustomAttributes(typeof(ParamValueAttribute), false);
-
-            return paramValue.Length > 0 ? paramValue[0].Value : value.ToString();
-        }
-
-        /// <summary>
         /// Converts DateTime to unix time (defaults to milliseconds).
         /// </summary>
         /// <param name="date">DateTime to convert.</param>
