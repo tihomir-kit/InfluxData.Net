521742c8873cf7274188f4472d276e882d246838
hanssens <github@hanssens.com>
2016-12-30 10:33:46 +0100

    Migrated project files to dotnet-core

--- src/InfluxData.Net.InfluxDb/Properties/AssemblyInfo.cs
+++ src/InfluxData.Net.InfluxDb/Properties/AssemblyInfo.cs
@@ -1,30 +1,19 @@
 ﻿using System.Reflection;
-using System.Resources;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("InfluxData.Net.InfluxDb")]
-[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("InfluxData.Net.InfluxDb")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
-[assembly: InternalsVisibleTo("InfluxData.Net.Tests")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b8d98b6e-cf9f-464f-995d-9528062aed7a")]
