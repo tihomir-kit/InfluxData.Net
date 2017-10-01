af2ee554c2f3da6a948d8de6e370adf69145dc92
Joakim Hansson <joakim.hansson@tretton37.com>
2017-10-01 02:43:20 +0200

    Add helper method for creating parameterized queries
    - Added string extension to sanitize queries.

---
+++ InfluxData.Net.InfluxDb/Helpers/QueryHelpers.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using InfluxData.Net.Common;
+using System.Linq;
+
+namespace InfluxData.Net.InfluxDb.Helpers
+{
+    public static class QueryHelpers
+    {
+        public static string BuildParameterizedQuery(string query, object param)
+        {
+            var paramRegex = "@([A-Za-z0-9åäöÅÄÖ'_-]+)";
+
+            var matches = Regex.Matches(query, paramRegex);
+
+            Type t = param.GetType();
+            PropertyInfo[] pi = t.GetProperties();
+
+            foreach(Match match in matches) 
+            {
+                if (!pi.Any(x => match.Groups[0].Value.Contains(x.Name)))
+                    throw new ArgumentException($"Missing parameter value for {match.Groups[0].Value}");
+            }
+
+            foreach (var propertyInfo in pi)
+            {
+                var paramValue = propertyInfo.GetValue(param);
+                var paramType = paramValue.GetType();
+
+                if(!paramType.IsPrimitive && paramType != typeof(String))
+                    throw new NotSupportedException($"The type {paramType.Name} is not a supported query parameter type.");
+
+                var sanitizedParamValue = paramValue;
+
+                if(paramType == typeof(String)) {
+                    sanitizedParamValue = ((string)sanitizedParamValue).Sanitize();
+                }
+
+                while (Regex.IsMatch(query, $"@{propertyInfo.Name}"))
+                {
+                    var match = Regex.Match(query, $"@{propertyInfo.Name}");
+
+                    query = query.Remove(match.Index, match.Length);
+                    query = query.Insert(match.Index, $"{sanitizedParamValue}");
+                }
+            }
+
+            return query;
+        }
+    }
+}
---
+++ InfluxData.Net.Tests/InfluxDb/Helpers/QueryHelpersTests.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using InfluxData.Net.InfluxDb.Helpers;
+using Xunit;
+
+namespace InfluxData.Net.Tests
+{
+    [Trait("InfluxDb SerieExtensions", "Serie extensions")]
+    public class QueryHelpersTests
+    {
+        [Fact]
+        public void Building_Parameterized_Query_Returns_Correct_String()
+        {
+            var firstTag = "firstTag";
+            var firstTagValue = "firstTagValue";
+
+            var firstField = "firstField";
+            var firstFieldValue = "firstFieldValue";
+
+            var query = "SELECT * FROM fakeMeasurement " +
+                       $"WHERE {firstTag} = @FirstTagValue " +
+                       $"AND {firstField} = @FirstFieldValue";
+
+            var expectedNewQuery = "SELECT * FROM fakeMeasurement " +
+                                   $"WHERE {firstTag} = {firstTagValue} " +
+                                   $"AND {firstField} = {firstFieldValue}";
+
+            var actualNewQuery = QueryHelpers.BuildParameterizedQuery(
+            query,
+                new
+                {
+                    @FirstTagValue = firstTagValue,
+                    @FirstFieldValue = firstFieldValue
+                });
+
+            Assert.Equal(expectedNewQuery, actualNewQuery);
+        }
+
+
+        [Fact]
+        public void Using_Non_Primitive_And_Non_String_Type_In_Parameters_Throws_NotSupportedException()
+        {
+            var firstTag = "firstTag";
+            var firstTagValue = "firstTagValue";
+
+            var firstField = "firstField";
+
+            var query = "SELECT * FROM fakeMeasurement " +
+                       $"WHERE {firstTag} = @FirstTagValue " +
+                       $"AND {firstField} = @FirstFieldValue";
+
+            Func<string> func = new Func<string>(() =>
+            {
+                return QueryHelpers.BuildParameterizedQuery(
+                query,
+                new
+                {
+                    @FirstTagValue = firstTagValue,
+                    @FirstFieldValue = new List<string>() { "NOT ACCEPTED" }
+                });
+            });
+
+            Assert.Throws(typeof(NotSupportedException), func);
+        }
+
+        [Fact]
+        public void Building_Parameterized_Query_With_Missing_Parameters_Throws_ArgumentException()
+        {
+            var firstTag = "firstTag";
+            var firstTagValue = "firstTagValue";
+
+            var firstField = "firstField";
+
+            var query = "SELECT * FROM fakeMeasurement " +
+                       $"WHERE {firstTag} = @FirstTagValue " +
+                       $"AND {firstField} = @FirstFieldValue";
+
+            Func<string> func = new Func<string>(() =>
+            {
+                return QueryHelpers.BuildParameterizedQuery(
+                query,
+                new
+                {
+                    @FirstTagValue = firstTagValue
+                });
+            });
+
+            Assert.Throws(typeof(ArgumentException), func);
+        }
+    }
+}
