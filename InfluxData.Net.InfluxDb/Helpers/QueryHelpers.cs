0d12aba7ca977ca5a8f080ff9d228d1ee5927f84
Joakim Hansson <joakim.hansson@tretton37.com>
2017-10-02 20:26:57 +0200

    Correct regex to match only full words

--- InfluxData.Net.InfluxDb/Helpers/QueryHelpers.cs
+++ InfluxData.Net.InfluxDb/Helpers/QueryHelpers.cs
@@ -10,36 +10,36 @@ namespace InfluxData.Net.InfluxDb.Helpers
     {
         public static string BuildParameterizedQuery(string query, object param)
         {
-            var paramRegex = "@([A-Za-z0-9åäöÅÄÖ'_-]+)";
-
-            var matches = Regex.Matches(query, paramRegex);
-
             Type t = param.GetType();
             PropertyInfo[] pi = t.GetProperties();
 
-            foreach(Match match in matches) 
-            {
-                if (!pi.Any(x => match.Groups[0].Value.Contains(x.Name)))
-                    throw new ArgumentException($"Missing parameter value for {match.Groups[0].Value}");
-            }
 
             foreach (var propertyInfo in pi)
             {
+                var regex = $@"@{propertyInfo.Name}(?!\w)";
+
+                if(!Regex.IsMatch(query, regex) && Nullable.GetUnderlyingType(propertyInfo.GetType()) != null)
+                    throw new ArgumentException($"Missing parameter identifier for @{propertyInfo.Name}");
+
                 var paramValue = propertyInfo.GetValue(param);
+                if (paramValue == null)
+                    continue;
+
                 var paramType = paramValue.GetType();
 
-                if(!paramType.IsPrimitive && paramType != typeof(String))
+                if (!paramType.IsPrimitive && paramType != typeof(String) && paramType != typeof(DateTime))
                     throw new NotSupportedException($"The type {paramType.Name} is not a supported query parameter type.");
 
                 var sanitizedParamValue = paramValue;
 
-                if(paramType == typeof(String)) {
+                if (paramType == typeof(String))
+                {
                     sanitizedParamValue = ((string)sanitizedParamValue).Sanitize();
                 }
 
-                while (Regex.IsMatch(query, $"@{propertyInfo.Name}"))
+                while (Regex.IsMatch(query, regex))
                 {
-                    var match = Regex.Match(query, $"@{propertyInfo.Name}");
+                    var match = Regex.Match(query, regex);
 
                     query = query.Remove(match.Index, match.Length);
                     query = query.Insert(match.Index, $"{sanitizedParamValue}");
